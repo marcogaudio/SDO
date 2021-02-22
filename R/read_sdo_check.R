5e74aa06b7dd19fd3fe6beca1fd9b48bb65e9ac7
marcogaudio <marcogaudio1997@outlook.it>
2021-02-22 09:53:42 +0100

    syntax changes.

--- R/read_sdo_check.R
+++ R/read_sdo_check.R
@@ -19,14 +19,6 @@ read_sdo_check <- function(x, y, year) {
   
 }
 
-
-# example
-file_path <- "./data/C_17_tavole_34_2_0_file.xlsx" 
-raw_sheets <- readxl::excel_sheets(file_path)[64]
-
-read_sdo_check(x = raw_sheets, y = file_path, year = "2019") 
-
-
 # get the table to match for all the 4 files.
 files_names = list.files(path = "./data/", pattern = ".xlsx",
                          full.names = TRUE)
@@ -37,7 +29,6 @@ Years <- list("2016", "2017", "2018", "2019")
 matching_tables <- 
   purrr::pmap(list(sheets_names, files_names, Years), read_sdo_check) %>%
   dplyr::bind_rows() %>%
-  dplyr::mutate(Attività = "Acuti - Regime ordinario") %>%
-  tibble::view()
+  dplyr::mutate(Attività = "Acuti - Regime ordinario") 
 
 
