876276a8b2b040404282413842b9698784260690
marcogaudio <marcogaudio1997@outlook.it>
2021-02-26 12:24:54 +0100

    using sdo_grid() function to automate stuff.

--- R/read_sdo_diurno.R
+++ R/read_sdo_diurno.R
@@ -22,31 +22,48 @@ read_sdo_diurno <- function(x, y, year) {
 }
 
 # get the sheets name of all the 4 files
-all_raw_sheets <- map(files_names, excel_sheets)
+# all_raw_sheets <- map(files_names, excel_sheets)
+# 
+# 
+# sheets_name_2016 <- c(all_raw_sheets[[1]][73:89])   
+# sheets_name_2017 <- c(all_raw_sheets[[2]][111:127]) 
+# sheets_name_2018 <- c(all_raw_sheets[[3]][111:127])  
+# sheets_name_2019 <- c(all_raw_sheets[[4]][111:127])  
+# 
+# 
+# Years <- c("2016", "2017", "2018", "2019")
+# 
+# first_grid  <- expand.grid(sheets_name_2016, files_names[1], Years[1])
+# second_grid <- expand.grid(sheets_name_2017, files_names[2], Years[2])
+# third_grid  <- expand.grid(sheets_name_2018, files_names[3], Years[3])
+# fourth_grid <- expand.grid(sheets_name_2019, files_names[4], Years[4])
+# 
+# total_grid <- dplyr::bind_rows(first_grid,
+#                                second_grid,
+#                                third_grid,
+#                                fourth_grid)
 
 
-sheets_name_2016 <- c(all_raw_sheets[[1]][73:89])   
-sheets_name_2017 <- c(all_raw_sheets[[2]][111:127]) 
-sheets_name_2018 <- c(all_raw_sheets[[3]][111:127])  
-sheets_name_2019 <- c(all_raw_sheets[[4]][111:127])  
+# use function sdo_grid() to automate stuff.
+files_names <- as.list(list.files(path = "./data/", pattern = ".xlsx",
+                                  full.names = TRUE))
 
 
-Years <- c("2016", "2017", "2018", "2019")
+# apply the function for all the 4 years.
 
-first_grid  <- expand.grid(sheets_name_2016, files_names[1], Years[1])
-second_grid <- expand.grid(sheets_name_2017, files_names[2], Years[2])
-third_grid  <- expand.grid(sheets_name_2018, files_names[3], Years[3])
-fourth_grid <- expand.grid(sheets_name_2019, files_names[4], Years[4])
+Years <- list("2016", "2017", "2018", "2019")
 
-total_grid <- dplyr::bind_rows(first_grid,
-                               second_grid,
-                               third_grid,
-                               fourth_grid)
+all_raw_sheets <- as.character(readxl::excel_sheets(path = files_names[[1]]))
+
+sheet_names <- all_rawss[str_detect(all_rawss, "Tav_2.2.12")]
+
+grid_list <- pmap(list(files_names, Years), sdo_grid) %>%
+  bind_rows()
 
 # function pmap takes on a list of lists.
-list_a <- as.list(as.character(total_grid$Var1))
-list_b <- as.list(as.character(total_grid$Var2))
-list_c <- as.list(as.character(total_grid$Var3))
+list_a <- as.list(as.character(grid_list$Var1))
+list_b <- as.list(as.character(grid_list$Var2))
+list_c <- as.list(as.character(grid_list$Var3))
 
 
 all_cleaned_datas <- purrr::pmap(list(list_a, list_b, list_c), read_sdo_diurno)
@@ -54,6 +71,6 @@ all_cleaned_datas <- purrr::pmap(list(list_a, list_b, list_c), read_sdo_diurno)
 merged_data_2.12 <- dplyr::bind_rows(all_cleaned_datas) %>% 
   view()
 
+merged_data_2.12_new <- dplyr::bind_rows(all_cleaned_datas)
 
-
-
+identical(merged_data_2.12, merged_data_2.12_new)
