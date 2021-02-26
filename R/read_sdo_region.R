f23f70e6dc68061fa8b4b67bf065eb2b8aff2f29
marcogaudio <marcogaudio1997@outlook.it>
2021-02-26 11:53:45 +0100

    using function sdo_grid() to automate stuff

--- R/read_sdo_region.R
+++ R/read_sdo_region.R
@@ -51,35 +51,46 @@ read_sdo_region <- function(x, y, year, region){
 # get the names of regions.
 Regioni <- c(HR_age$`REGIONE DI RESIDENZA`[1:21])
 
+files_names <- as.list(list.files(path = "./data/", pattern = ".xlsx",
+                                  full.names = TRUE))
+
+
 # apply the function for all the 4 years.
 
-Years <- c("2016", "2017", "2018", "2019")
+Years <- list("2016", "2017", "2018", "2019")
 
-all_raw_sheets <- map(files_names, excel_sheets)
+all_raw_sheets <- as.character(readxl::excel_sheets(path = files_names[[1]]))
 
+sheet_names <- all_rawss[str_detect(all_rawss, "Tav_8.1")]
 
-sheets_name_2016 <- c(all_raw_sheets[[1]][317:337])   
-sheets_name_2017 <- c(all_raw_sheets[[2]][358:378]) 
-sheets_name_2018 <- c(all_raw_sheets[[3]][358:378])  
-sheets_name_2019 <- c(all_raw_sheets[[4]][358:378])  
+# write a function to replace the previous code.
+# the function will expand the grid, then use pmap to apply for all the years.
+sdo_grid <- function(file, year, area){
+  
+  grid <- cbind(expand.grid(sheet_names, file, year), area)
+  
+}
 
+grid_list <- pmap(list(files_names, Years), sdo_grid, area = Regioni) %>%
+  bind_rows()
 
-first_grid  <- cbind(expand.grid(sheets_name_2016, files_names[1], Years[1]), Regioni)
-second_grid <- cbind(expand.grid(sheets_name_2017, files_names[2], Years[2]), Regioni)
-third_grid  <- cbind(expand.grid(sheets_name_2018, files_names[3], Years[3]), Regioni)
-fourth_grid <- cbind(expand.grid(sheets_name_2019, files_names[4], Years[4]), Regioni)
-
-total_grid <- dplyr::bind_rows(first_grid,
-                               second_grid,
-                               third_grid,
-                               fourth_grid)
+# previous code.
+# first_grid  <- cbind(expand.grid(sheet_names, files_names[1], Years[1]), Regioni)
+# second_grid <- cbind(expand.grid(sheets_name_2017, files_names[2], Years[2]), Regioni)
+# third_grid  <- cbind(expand.grid(sheets_name_2018, files_names[3], Years[3]), Regioni)
+# fourth_grid <- cbind(expand.grid(sheets_name_2019, files_names[4], Years[4]), Regioni)
+# 
+# total_grid <- dplyr::bind_rows(first_grid,
+#                                second_grid,
+#                                third_grid,
+#                                fourth_grid)
 
 # function pmap takes on a list of lists.
 
-list_a <- as.list(as.character(total_grid$Var1))
-list_b <- as.list(as.character(total_grid$Var2))
-list_c <- as.list(as.character(total_grid$Var3))
-list_d <- as.list(as.character(total_grid$Regioni))
+list_a <- as.list(as.character(grid_list$Var1))
+list_b <- as.list(as.character(grid_list$Var2))
+list_c <- as.list(as.character(grid_list$Var3))
+list_d <- as.list(as.character(grid_list$area))
 
 region_datas <- purrr::pmap(list(list_a, list_b, list_c, list_d), read_sdo_region)
 
