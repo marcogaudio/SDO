3409e5f619efcaffed14919e4376e55fc50bbb7c
marcogaudio <marcogaudio1997@outlook.it>
2021-03-02 11:31:48 +0100

    read table "HES crude results" for the UK number of surgeries and surgical incidence.

---
+++ R/UK_surgery.R
@@ -0,0 +1,33 @@
+# read table "HES crude results" for the UK number of surgeries and surgical incidence.
+
+file_path <- list.files( path = "./data/UK", pattern = ".xlsx",
+                         full.names = TRUE)
+
+raw_data <- readxl::read_xlsx(path = file_path, 
+                              sheet = "HES crude results", skip = 3)
+view(raw_data)
+
+
+
+# the following tibble represents the total number of surgery in England(2019)
+# is the year correct? 
+
+UK_surgery <- raw_data %>%
+  dplyr::slice(1:18) %>%
+  dplyr::select(1:3) %>%
+  dplyr::rename(`AGE CATEGORY` = "Age...1",
+                MALE = "Male...2",
+                FEMALE = "Female...3") %>%
+  dplyr::mutate(YEAR = "2019")
+
+
+# the following tibble represents the surgical incidences in England(2019)
+
+UK_surgery_incidence <- raw_data %>%
+  dplyr::slice(1:18) %>%
+  dplyr::select(5:7) %>%
+  dplyr::rename(`AGE CATEGORY` = "Age...5",
+                MALE = "Male...6",
+                FEMALE = "Female...7") %>%
+  dplyr::mutate(YEAR = "2019")
+
